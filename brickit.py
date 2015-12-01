312a2cc4d5fb696e08e000b8896e734fb0d4df77
Aaron Kurtz <a.kurtz@hardsun.net>
2015-12-01 12:04:02 -0800

    Limit keen events to live, report bad uploads

--- brickit.py
+++ brickit.py
@@ -8,8 +8,14 @@ from flask import Flask, render_template, request, redirect, send_file
 import keen
 import legofy
 
+DEV = 'development'
+LIVE = 'production'
+
 BRICK_PATH = os.path.join(os.path.dirname(legofy.__file__), "assets", "bricks", "1x1.png")
 BRICK_IMAGE = Image.open(BRICK_PATH)
+
+
+BRICKY_ENV = os.environ.get('BRICKY_ENV', DEV)
 
 app = Flask(__name__)
 
@@ -31,9 +37,13 @@ def upload():
     uploaded = request.files['file']
     if not uploaded:
         return redirect('/')
+    file_info = {'filename': uploaded.filename, 'mimetype': uploaded.mimetype}
     try:
         image = Image.open(uploaded)
     except IOError:
+        app.logger.info('Bad Upload - {}'.format(file_info))
+        if BRICKY_ENV == LIVE:
+            keen.add_event('bad_upload', file_info)
         return redirect('/')
     new_size = legofy.get_new_size(image, BRICK_IMAGE)
     image.thumbnail(new_size, Image.ANTIALIAS)
@@ -44,11 +54,10 @@ def upload():
 
     browser_info = {'user_agent': request.headers.get('User-Agent', 'No Agent'),
                     'X-Request-ID': request.headers.get('X-Request-ID', 'Missing X-Request-ID')}
-    file_info = {'filename': uploaded.filename, 'mimetype': uploaded.mimetype}
 
     app.logger.info('Headers - {}'.format(browser_info))
     app.logger.info('Upload - {}'.format(file_info))
-    if not app.debug:
+    if BRICKY_ENV == LIVE:
         keen.add_event('browser', browser_info)
         keen.add_event('upload', file_info)
 
