5cd95ac46d4be313e834b1d01958f9b15c563752
Aaron Kurtz <a.kurtz@hardsun.net>
2015-12-01 01:46:51 -0800

    Add Legofy

--- brickit.py
+++ brickit.py
@@ -1,5 +1,11 @@
 import os
+
+import legofy
+import PIL
 from flask import Flask
+
+brick_path = os.path.join(os.path.dirname(legofy.__file__), "assets", "bricks", "1x1.png")
+brick_image = PIL.Image.open(brick_path)
 
 app = Flask(__name__)
 
